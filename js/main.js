c6491977879be0f5ee0aee4b8285a1715baffef7
Clark <c_clark20@tss-hr420-dm03.local>
2017-12-04 17:27:55 -0500

    Got the PHP working

--- js/main.js
+++ js/main.js
@@ -1,29 +1,54 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-   var carImages = document.querySelectorAll('.data-ref'),
-   modelName = document.querySelector('.modelName'),
-   priceInfo = document.querySelector('.priceInfo'),
-   modelDetails = document.querySelector('.modelDetails'),
-   appliedClass;
+   var carImages = document.querySelectorAll('.data-ref');
+
+   const httpRequest = new XMLHttpRequest();
 
    function changeText() {
-     let objectIndex = carData [this.id];
+        //make an AJAX call to the database; handle errors first
+        if (!httpRequest) {
+          alert('giving up... your browser sucks');
+          return false;
+        }
 
-     //change the text
-          //change the model name
-              modelName.firstChild.nodeValue = objectIndex.model;
-          //change the price
-              priceInfo.firstChild.nodeValue = objectIndex.price;
-          //change the description
-              modelDetails.firstChild.nodeValue = objectIndex.desc;
+        httpRequest.onreadystatechange = processRequest;
+        httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
+        httpRequest.send();
+     }
+
+
+  function processRequest() {
+    let reqIndicator = document.querySelector('.request-state');
+    reqIndicator.textContent = httpRequest.readyState;
+    debugger;
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) { // 200 means everything is awesome
+        debugger;
+        let data = JSON.parse(httpRequest.responseText);
+
+        processResult(data);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+   function processResult(data) {
+     const { modelName, pricing, modelDetails } = data;
+     let model = document.querySelector('.modelName').textContent = modelName;
+     let price = document.querySelector('.priceInfo').innerHTML = pricing;
+     let desc = document.querySelector('.modelDetails').textContent = modelDetails;
+
+     let objectIndex = carData [this.id];
 
       //change opacity
       carImages.forEach(function(image, index){
           image.classList.add("nonActive");
       });
 
+      document.querySelector(`#${data.model}`).classList.remove('nonActive');
       //remove opacity when clicked
-      this.classList.remove('nonActive');
+      //this.classList.remove('nonActive');
    }
 
 
@@ -33,6 +58,7 @@
 
    //Preload low opacity
    changeText.call(document.querySelector('#F55'));
+
 
 
 })();
